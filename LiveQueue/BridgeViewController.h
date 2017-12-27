709849f62c79990f1de5cd3b7620a145bc700b19
Jordan Siff <jordansiff@jordans-macbook-pro.local>
2017-12-27 17:18:06 -0500

    Authentication

---
+++ LiveQueue/BridgeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BridgeViewController.h
+//  LiveQueue
+//
+//  Created by Jordan Siff on 12/27/17.
+//  Copyright © 2017 Jordan Siff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BridgeViewController : UIViewController
+
+@end
--- LiveQueue/ViewController.swift
+++ LiveQueue/ViewController.swift
@@ -15,11 +15,44 @@ class TableViewController: UITableViewController {
     var searchURL = "https://api.spotify.com/v1/search?q=Kanye+West&type=track"
     
     typealias JSONStandard = [String : AnyObject]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         callAlamo(url: searchURL)
+        setup()
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.updateAfterFirstLogin))
     }
+    
+    func updateAfterFirstLogin () {
+        if let sessionObj:AnyObject = userDefaults.object(forKey: "SpotifySession") as AnyObject? {
+            let sessionDataObj = sessionObj as! Data
+            let firstTimeSession = NSKeyedUnarchiver.unarchiveObject(with: sessionDataObj) as! SPTSession
+            self.session = firstTimeSession
+            initializePlayer(authSession: session)
+        }
+        
+    func initializePlayer(authSession:SPTSession){
+        if self.player == nil {
+            self.player = SPTAudioStreamingController.sharedInstance()
+            self.player!.playbackDelegate = self
+            self.player!.delegate = self
+            try! player!.start(withClientId: auth.clientID)
+            self.player!.login(withAccessToken: authSession.accessToken)
+            }
+        }
+    
+    func audioStreamingDidLogin(_ audioStreaming: SPTAudioStreamingController!) {
+        // after a user authenticates a session, the SPTAudioStreamingController is then initialized and this method called
+        print("logged in")
+        self.player?.playSpotifyURI("spotify:track:58s6EuEYJdlb0kO7awm3Vp", startingWith: 0, startingWithPosition: 0, callback: { (error) in
+            if (error != nil) {
+                print("playing!")
+            }
+        })
+    }
+        
+        
 
     func callAlamo(url : String){
         Alamofire.request(url).responseJSON(completionHandler: {
@@ -67,7 +100,30 @@ class TableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Authentication
+    
+    var auth = SPTAuth.defaultInstance()!
+    var session:SPTSession!
+    var player: SPTAudioStreamingController?
+    var loginUrl: URL?
+    //let clientID  = "5820fd69400947cb87dc2784c9dd5afc"
+    
+    func setup() {
+        SPTAuth.defaultInstance().clientID = "5820fd69400947cb87dc2784c9dd5afc"
+        SPTAuth.defaultInstance().redirectURL = URL(string : "LiveQueue://returnAfterLogin")
+        SPTAuth.defaultInstance().requestedScopes = [SPTAuthStreamingScope, SPTAuthPlaylistReadPrivateScope, SPTAuthPlaylistModifyPublicScope, SPTAuthPlaylistModifyPrivateScope]
+        loginUrl = SPTAuth.defaultInstance().spotifyWebAuthenticationURL()
+    }
 
-
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        if UIApplication.shared.openURL(loginUrl!) {
+            if auth.canHandle(auth.redirectURL) {
+                // To do - build in error handling
+            }
+        }
+    }
+    
 }
 
+}
